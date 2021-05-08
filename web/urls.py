c932715316abba6d430dcc5a92165e2ced58ca6a
oceaster <oceaster@gmail.com>
2021-05-08 14:51:16 +0100

    08 MAY 2021

--- web/urls.py
+++ web/urls.py
@@ -1,77 +1,13 @@
 # Essential
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 
 # Application Views
+from api.pardoewray.urls import urlpatterns as PW
 from api.views import *
 from core.views import *
 from tools.server.api import *
 
 # URL Endpoints
 urlpatterns = [
-
-    # Global App
-    path('', index),
-    path('index.html', index),
-    path('robots.txt', robots),
-    path('manifest.json', manifestJSON),
-    path('asset-manifest.json', asset_manifest),
-
-    # Global App Service Worker
-    path('service-worker.js', service_worker),
-    path('service-worker.js.map', service_worker_map),
-
-    # Chat App
-    path('messages', chat.app),
-    path('echat.app', chat.app),
-    path('echat/manifest.json', chat.manifestJSON),
-    path('echat/asset-manifest.json', chat.asset_manifest),
-
-    # Inverair App
-    path('inverair.app', inverair_app),
-    path('inverair/manifest.json', inverair_manifestJSON),
-    path('inverair/asset-manifest.json', inverair_asset_manifest),
-
-    # Pardoewray App
-    path('pardoewray/manifest.json', pardoewray_manifestJSON),
-    path('pardoewray/asset-manifest.json', pardoewray_asset_manifest),
-    re_path(r'pardoewray/.*$', pardoewray_app, name='pardoewray'),
-
-    # Pardoewray API
-    path('api/news', pardoewrayAPI.fetch_news),
-    path('api/jobs', pardoewrayAPI.fetch_job_postings),
-    path('api/news/<str:pid>', pardoewrayAPI.fetch_post),
-    path('api/jobs/<str:uid>', pardoewrayAPI.fetch_job_detail),
-    path(
-        'api/jobs/apply/<str:uid>/<str:fname>/<str:lname>/<str:umail>/<str:number>',
-        pardoewrayAPI.user_apply
-    ),
-    path('api/admin/news/create/<str:title>/<str:content>', pardoewrayAPI.create_news),
-    path('api/admin/jobs', pardoewrayAPI.fetch_job_postings_admin),
-    path('api/admin/jobs/delete/<str:uid>', pardoewrayAPI.delete_job_post_admin),
-    path(
-        'api/admin/jobs/create/<str:title>/<str:company>/<str:website>/<str:location>/<str:jType>',
-        pardoewrayAPI.create_new_post
-    ),
-    path(
-        'api/admin/jobs/update/desc/<str:uid>/<str:description>',
-        pardoewrayAPI.update_post_desc
-    ),
-    path(
-        'api/admin/jobs/update/reqs/<str:uid>/<str:reqs>',
-        pardoewrayAPI.update_post_reqs
-    ),
-
-    # OLT Server Admin API
-    path('api/olt/status', OLT_status_api),
-    path('api/olt/upgrade', OLT_upgrade_request_api),
-
-    # Login API
-    path('api/login', login.views.user),
-    path('api/admin', login.views.admin),
-    path('api/login/verify', login.views.verify),
-
-    # Register API
-    path('api/register', new_user.views.register_new_user),
-    path('api/register/verify', new_user.views.check_email_exists)
-
+    path(r'pardoewray/', include(PW))
 ]
